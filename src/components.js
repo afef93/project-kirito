fb9f8affc27adb96e6ec19cc43af55e4d9fa5be8
Afef Hedia <afef@celaneo.com>
2020-10-30 22:17:09 +0100

    first commit

--- src/components.js
+++ src/components.js
@@ -18,17 +18,7 @@ export function Footer() {
     return (
         <div>
             <p>
-        <span className="credits center">
-          <span>Made by </span>
-          <a
-              href="https://github.com/MichalSzorad"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="center"
-          >
-            <span>@MichalSzorad</span>
-          </a>
-        </span>
+                Afef Hedia
             </p>
         </div>
     );
